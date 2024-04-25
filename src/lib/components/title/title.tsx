237a95efe596057c99561fe3a366d0be79a7f07a
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2024-04-24 20:44:41 -0400

    remove github logo

--- src/lib/components/title/title.tsx
+++ src/lib/components/title/title.tsx
@@ -13,13 +13,6 @@ function Title({ title }: Props) {
         <Home size={24} />
       </Link>
       {title}
-      <a href="https://github.com/sadanandpai/algo-visualizers" target="blank">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-          alt="github repo"
-          className={classes.github}
-        />
-      </a>
     </h1>
   );
 }
